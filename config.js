df407cdddf83fe600dff38844f9b5838e58d1e3f
sathviru <78941711+sathviru@users.noreply.github.com>
2023-09-26 16:54:40 +0530

    Update config.js

--- config.js
+++ config.js
@@ -22,14 +22,14 @@ global.APIKeys = {
 ////////////////////////////////////////////////////|:|             OWNER DETAILS             |:|//////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 global.session = process.env.SESSION_ID || 'MDdXRGdiUjM=',
-global.ownername = process.env.OWNER_NAME === undefined ? "Dark Sathviru " : process.env.OWNER_NAME
-let fake = process.env.OWNER_NUMBER === undefined ? '94701416588' : process.env.OWNER_NUMBER
+global.ownername = process.env.OWNER_NAME === undefined ? "sathviru " : process.env.OWNER_NAME
+let fake = process.env.OWNER_NUMBER === undefined ? '94721489840' : process.env.OWNER_NUMBER
 global.owner = [`${fake}`]
 global.ownernumber = [`${fake}`]
 global.premium = [`${fake}`]
 global.anticall = process.env.ANTICALL === undefined ? 'false' : process.env.ANTICALL
 global.packname = process.env.PACK_NAME === undefined ? 'Queen-MD' : process.env.PACK_NAME
-global.author = process.env.STICKER_AUTHOR_NAME === undefined ? 'Dark sathviru ' : process.env.STICKER_AUTHOR_NAME
+global.author = process.env.STICKER_AUTHOR_NAME === undefined ? 'sathviru ' : process.env.STICKER_AUTHOR_NAME
 global.pmblock = process.env.PM_BLOCK === undefined ? 'false' : process.env.PM_BLOCK
 global.chatbot_pm = process.env.PM_CHATBOT === undefined ? 'true' : process.env.PM_CHATBOT
 global.chatbot_group = process.env.GROUP_CHATBOT === undefined ? 'true' : process.env.GROUP_CHATBOT
